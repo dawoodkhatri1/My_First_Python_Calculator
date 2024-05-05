4402255a45b02706cc8bff12c19e995750d99384
dawoodkhatri1 <136968266+dawoodkhatri1@users.noreply.github.com>
2024-05-05 11:23:39 +0500

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,21 @@
+val1 = 23
+val2 = 5
+
+# addition
+print("The addition of", val1, "and", val2, "is: ", val1+val2)
+
+# subtraction
+print("The subtraction of", val1, "and", val2, "is: ", val1-val2)
+
+# multiplication
+print("The multiplication of", val1, "and", val2, "is: ", val1*val2)
+
+# division
+print("The division of", val1, "and", val2, "is: ", val1/val2)
+print("The whole division of", val1, "and", val2, "is: ", val1//val2)
+
+# exponential
+print("The exponential of", val1, "and", val2, "is: ", val1**val2)
+
+# modulus
+print("The modulud of", val1, "and", val2, "is: ", val1%val2)
